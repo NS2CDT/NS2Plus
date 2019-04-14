dc30241a6a5eb6f068b8c5ff6ae8ad286baa1c72
GhoulofGSG9 <ghoulofgsg9@gmail.com>
2019-04-14 17:19:21 +0200

    Removed custom unit status icons Vanilla has those now

--- lua/NS2Plus/Client/CHUDGUI_HiddenViewmodel.lua
+++ lua/NS2Plus/Client/CHUDGUI_HiddenViewmodel.lua
@@ -1,48 +1,9 @@
 class 'CHUDGUI_HiddenViewmodel' (GUIScript)
 
-local fireTexture = PrecacheAsset("ui/chud_fireindicator.dds")
-local obsIndicatorTexture = PrecacheAsset("ui/chud_scanindicator.dds")
-local obsTextureCoords = {0, 0, 64, 64}
-local buildTexture = PrecacheAsset("ui/buildmenu.dds")
-local iconSize, exoHUDSize
+local exoHUDSize
 
 function CHUDGUI_HiddenViewmodel:Initialize()
-
-	iconSize = GUIScale(Vector(64, 64, 0))
 	exoHUDSize = GUIScale(Vector(160, 160, 0))
-
-	self.fireIndicator = GUIManager:CreateGraphicItem()
-	self.fireIndicator:SetAnchor(GUIItem.Left, GUIItem.Bottom)
-	self.fireIndicator:SetLayer(kGUILayerPlayerHUD)
-	self.fireIndicator:SetIsVisible(false)
-	self.fireIndicator:SetTexture(fireTexture)
-	self.fireIndicator:SetSize(iconSize)
-	self.fireIndicator:SetColor(kAlienFontColor)
-
-	self.leftIndicator = GUIManager:CreateGraphicItem()
-	self.leftIndicator:SetAnchor(GUIItem.Left, GUIItem.Bottom)
-	self.leftIndicator:SetLayer(kGUILayerPlayerHUD)
-	self.leftIndicator:SetIsVisible(false)
-	self.leftIndicator:SetColor(kAlienFontColor)
-	self.leftIndicator:SetSize(iconSize)
-
-	self.umbraIndicator = GUIManager:CreateGraphicItem()
-	self.umbraIndicator:SetAnchor(GUIItem.Right, GUIItem.Bottom)
-	self.umbraIndicator:SetLayer(kGUILayerPlayerHUD)
-	self.umbraIndicator:SetIsVisible(false)
-	self.umbraIndicator:SetTexture(buildTexture)
-	self.umbraIndicator:SetTexturePixelCoordinates(GUIUnpackCoords(GetTextureCoordinatesForIcon(kTechId.Umbra)))
-	self.umbraIndicator:SetSize(iconSize)
-	self.umbraIndicator:SetColor(kAlienFontColor)
-	
-	self.enzymeIndicator = GUIManager:CreateGraphicItem()
-	self.enzymeIndicator:SetAnchor(GUIItem.Right, GUIItem.Bottom)
-	self.enzymeIndicator:SetLayer(kGUILayerPlayerHUD)
-	self.enzymeIndicator:SetIsVisible(false)
-	self.enzymeIndicator:SetTexture(buildTexture)
-	self.enzymeIndicator:SetTexturePixelCoordinates(GUIUnpackCoords(GetTextureCoordinatesForIcon(kTechId.EnzymeCloud)))
-	self.enzymeIndicator:SetSize(iconSize)
-	self.enzymeIndicator:SetColor(kAlienFontColor)
 
 	-- Set the texture to a temp one, avoids crash
 	self.leftExo = GUIManager:CreateGraphicItem()
@@ -63,59 +24,9 @@ end
 
 local lastLeft, lastRight, lastChange
 function CHUDGUI_HiddenViewmodel:Update(_)
-	local alienHUDScript = ClientUI.GetScript("GUIAlienHUD")
 	local exoHUDScript = ClientUI.GetScript("Hud/Marine/GUIExoHUD")
-	local player = Client.GetLocalPlayer()
-	if alienHUDScript then
-		local healthBall = alienHUDScript.healthBall:GetBackground()
-		local energyBall = alienHUDScript.energyBall:GetBackground()
-		local size = healthBall:GetSize()
-		local leftPos = healthBall:GetPosition() + Vector(size.x-GUIScale(16), 0, 0)
-		local rightPos = energyBall:GetPosition() - Vector(size.x-GUIScale(48), 0, 0)
-		
-		local fire = player:GetIsOnFire()
-		local obs = player:GetIsDetected()
-		local cloak = player:GetCloakFraction() > 0.2
-		local umbra = player.umbraIntensity > 0
-		local enzyme = player:GetIsEnzymed()
-		
-		self.fireIndicator:SetIsVisible(player:GetIsAlive() and fire)
-		-- When under obs/scan, it removes cloaking, so only one of them happen at a time
-		self.leftIndicator:SetIsVisible(player:GetIsAlive() and (obs or cloak))
-		self.umbraIndicator:SetIsVisible(player:GetIsAlive() and umbra)
-		self.enzymeIndicator:SetIsVisible(player:GetIsAlive() and enzyme)
-		
-		if fire then
-			self.fireIndicator:SetPosition(leftPos)
-			leftPos = leftPos + Vector(iconSize.x, 0, 0)
-		end
-
-		self.leftIndicator:SetPosition(leftPos)
-
-		if obs then
-			self.leftIndicator:SetTexture(obsIndicatorTexture)
-			self.leftIndicator:SetTexturePixelCoordinates(GUIUnpackCoords(obsTextureCoords))
-			self.leftIndicator:SetColor(Color(1,1,1,1))
-		elseif cloak then
-			self.leftIndicator:SetTexture(buildTexture)
-			self.leftIndicator:SetTexturePixelCoordinates(GUIUnpackCoords(GetTextureCoordinatesForIcon(kTechId.Vampirism)))
-			self.leftIndicator:SetColor(kAlienFontColor)
-			
-			-- Do this to not override the original kAlienFontColor
-			local color = self.leftIndicator:GetColor()
-			color.a = player:GetCloakFraction()
-			self.leftIndicator:SetColor(color)
-		end
-		
-		if umbra then
-			self.umbraIndicator:SetPosition(rightPos)
-			rightPos = rightPos - Vector(iconSize.x, 0, 0)
-		end
-		
-		if enzyme then
-			self.enzymeIndicator:SetPosition(rightPos)
-		end
-	elseif exoHUDScript then
+	if exoHUDScript then
+		local player = Client.GetLocalPlayer()
 		local weapon = player:GetActiveWeapon()
 		if weapon and weapon:isa("ExoWeaponHolder") then
 			local leftWeapon = Shared.GetEntity(weapon.leftWeaponId)
